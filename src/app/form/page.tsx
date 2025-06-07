a941ad973c207e73cc77ed1d5224f72274efbe4b
もっちゃん <s7056i3056@gmail.com>
2025-06-07 19:42:01 +0900

    feat: ページ遷移テスト用のファイル作成

---
+++ src/app/form/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "入力フォーム",
+  description: "Playwrightハンズオンの第2のステップ",
+};
+
+export default function Form() {
+  return (
+    <main>
+      <h1>入力フォーム</h1>
+    </main>
+  );
+}
