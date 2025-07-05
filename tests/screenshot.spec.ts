384897f4e156ca430c07697d28adc7c3ee3a2be2
もっちゃん <s7056i3056@gmail.com>
2025-07-05 23:00:00 +0900

    test: full screenshot

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -2,5 +2,5 @@ import test from "@playwright/test";
 
 test("screenshot", async ({ page }) => {
   await page.goto("https://playwright.dev/");
-  await page.screenshot({ path: "screenshot.png" });
+  await page.screenshot({ path: "screenshot_full.png", fullPage: true });
 });
