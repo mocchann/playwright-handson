55d94e61bd41e67d5f4d306286756b1b5a601066
もっちゃん <s7056i3056@gmail.com>
2025-07-06 23:00:00 +0900

    test: screenshot element

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -2,5 +2,7 @@ import test from "@playwright/test";
 
 test("screenshot", async ({ page }) => {
   await page.goto("https://playwright.dev/");
-  await page.screenshot({ path: "screenshot_full.png", fullPage: true });
+  await page
+    .getByRole("heading", { name: "Playwright enables reliable" })
+    .screenshot({ path: "screenshot-element.png" });
 });
