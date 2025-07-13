5e9018f52ec4aaa56ecf474cd146d455d360f009
もっちゃん <s7056i3056@gmail.com>
2025-07-13 13:03:00 +0900

    test: GETリクエスト待機

--- tests/network.spec.ts
+++ tests/network.spec.ts
@@ -11,8 +11,17 @@ test("network", async ({ page }) => {
 
   await page.goto("https://example.com");
 });
+
 test("特定のURLリクエストを待機する場合", async ({ page }) => {
   const requestPromise = page.waitForRequest("**/api/fetch_data");
+  await page.getByText("trigger request").click();
+  const request = await requestPromise;
+});
+
+test("特定のURLへのGETリクエストを待機する場合", async ({ page }) => {
+  const requestPromise = page.waitForRequest(
+    (req) => req.url() === "**/api/fetch_data" && req.method() === "GET"
+  );
   await page.getByText("trigger request").click();
   const request = await requestPromise;
 });
